7c4727f9aecbcac556148e8a076110dbccc4de64
Ethan DeSon <ethandeson@gmail.com>
2018-04-25 10:49:54 -0700

    fixed bug where socket.io events would try to access MongoDB before it was loaded by making app run in MongoDB connection callback

--- app.js
+++ app.js
@@ -20,132 +20,135 @@ MongoClient.connect(mongo_uri, (err,client) => {
   if(!err){
     console.log("Connected to MongoDB");
     db = client.db(db_str);//
+    run_app();
   }else{
     console.log(err);
   }
 });
 
-
-//Handle GETS
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'static/index.html'));
-});
-
-app.get('/about', (req, res) => {
-  res.send('\n\nI made this for our book club!\n\n');
-
-});
-
-//Start socket.io server
-var server = app.listen(port, () => {
-  console.log('Server running at http://'+hostname+':'+port+'/');
-});
-const io = require('socket.io')(server);
-
-
-//TEMP DATA
-var speaker_str = "Default"; //Stores current speaker
-var current_users = [];
-
-//Handle server/client events:
-io.on('connection', function(socket) {
-
-   //Initiate a new connection
-   console.log("New Connection");
-   var welcome_str = 'Welcome!';
-   //Send welcome, speaker, and all saved comments from MongoDB database;
-   var comments = db.collection('comments');
-   comments.find({}).toArray((err, result) => {
-      if(err) throw err;
-      socket.emit('init', { 
-                            welcome: welcome_str, 
-                            comments: result, 
-                            speaker: speaker_str, 
-                            users: current_users
-                          });
-   });
-
-   socket.on('username_button', (data) => {
-     var entry = {
-                   'username': data.username
-                 }
-     var users = db.collection('users');
-     users.insert(entry); 
+function run_app(){
+   //Handle GETS
+   app.get('/', (req, res) => {
+     res.sendFile(path.join(__dirname,'static/index.html'));
    });
    
-   socket.on('round_button', (data) => {
-     var users = db.collection('users');
-     users.find({}).toArray((err, result) => {  
-        if(err) throw err;
-        if (result.length == 0){
-           data = { user: 'Default'};
-        } else {
-           var rand_index = Math.floor(Math.random()*result.length);
-           data = { user: result[rand_index]['username'] };
-        }
-        speaker_str = data.user; //Update global current speaker
-        io.sockets.emit('new_round', data); 
-     });
+   app.get('/about', (req, res) => {
+     res.send('\n\nI made this for our book club!\n\n');
+   
    });
-   //Handle comment submittions
-   socket.on('comment_button', (data) => {
-     var entry = {
-                  'user': data.user, 
-                  'comment' : data.comment
-                 };
-     var comments = db.collection('comments');
-     comments.insert(entry);
-     io.sockets.emit('new_comment', data);
+   
+   //Start socket.io server
+   var server = app.listen(port, () => {
+     console.log('Server running at http://'+hostname+':'+port+'/');
    });
-
-   //Initliaze pagenumber
-   var pagenumbers = db.collection('pagenumbers'); 
-   pagenumbers.find({}).sort({'pagenumbers': 1}).limit(1).toArray((err, result)=> {
+   const io = require('socket.io')(server);
+   
+   
+   //TEMP DATA
+   var speaker_str = "Default"; //Stores current speaker
+   var current_users = [];
+   
+   //Handle server/client events:
+   io.on('connection', function(socket) {
+   
+      //Initiate a new connection
+      console.log("New Connection");
+      var welcome_str = 'Welcome!';
+      //Send welcome, speaker, and all saved comments from MongoDB database;
+      var comments = db.collection('comments');
+      comments.find({}).toArray((err, result) => {
          if(err) throw err;
-         if (result.length == 0) {
-            data = { pagenumber: 0 }; 
-         }
-         else {
-            data = { pagenumber: result[0]['pagenumber'] };
-         }
-         io.sockets.emit('new_lowest_pagenumber', data);
-   });
-  
-   //Handle pagenumber submittions
-   socket.on('pagenumber_button', (data) => {
-     var pagenumber = data.pagenumber;
-     var entry = { 
-                  'pagenumber': pagenumber
-                 };
-     var pagenumbers = db.collection('pagenumbers'); 
-     pagenumbers.insert(entry);
-     pagenumbers.find({}).sort({'pagenumbers': 1}).limit(1).toArray((err, result)=> { 
-        var new_min = result[0]['pagenumber']; 
-        if(pagenumber <= new_min){
-           io.sockets.emit('new_lowest_pagenumber', data);
+         socket.emit('init', { 
+                               welcome: welcome_str, 
+                               comments: result, 
+                               speaker: speaker_str, 
+                               users: current_users
+                             });
+      });
+   
+      socket.on('username_button', (data) => {
+        var entry = {
+                      'username': data.username
+                    }
+        var users = db.collection('users');
+        users.insert(entry); 
+      });
+      
+      socket.on('round_button', (data) => {
+        var users = db.collection('users');
+        users.find({}).toArray((err, result) => {  
+           if(err) throw err;
+           if (result.length == 0){
+              data = { user: 'Default'};
+           } else {
+              var rand_index = Math.floor(Math.random()*result.length);
+              data = { user: result[rand_index]['username'] };
+           }
+           speaker_str = data.user; //Update global current speaker
+           io.sockets.emit('new_round', data); 
+        });
+      });
+      //Handle comment submittions
+      socket.on('comment_button', (data) => {
+        var entry = {
+                     'user': data.user, 
+                     'comment' : data.comment
+                    };
+        var comments = db.collection('comments');
+        comments.insert(entry);
+        io.sockets.emit('new_comment', data);
+      });
+   
+      //Initliaze pagenumber
+      var pagenumbers = db.collection('pagenumbers'); 
+      //Find and send min page number
+      pagenumbers.find({}).sort({'pagenumbers': 1}).limit(1).toArray((err, result)=> {
+            if(err) throw err;
+            if (result.length == 0) {
+               data = { pagenumber: 0 }; 
+            }
+            else {
+               data = { pagenumber: result[0]['pagenumber'] };
+            }
+            io.sockets.emit('new_lowest_pagenumber', data);
+      });
+     
+      //Handle pagenumber submittions
+      socket.on('pagenumber_button', (data) => {
+        //BROKEN BECAUSE pagenumber_button events are affecting each other
+        var pagenumber = data.pagenumber;
+        var entry = { 
+                     'pagenumber': pagenumber
+                    };
+        var find_and_emit_min = () => { 
+           pagenumbers.find({}).sort({'pagenumbers': 1}).limit(1).toArray((err, result)=> { 
+              var new_min = result[0]['pagenumber']; 
+              if(pagenumber <= new_min){
+                 io.sockets.emit('new_lowest_pagenumber', data);
+              }
+           });
         }
-     });
-   });
-
-   socket.on('pagenumber_reset_button', (data) => {
-    var pagenumbers = db.collection('pagenumbers');   
-    //Delete all stored pagenumbers
-    pagenumbers.remove({}, (err, obj) => {});
-    
-   });
-  
-   //Handle new logins
-   socket.on('login', (data) => {
-    current_users.push(data.user);
-    io.sockets.emit('new_user', data);
-   });
-   socket.on('signout', (data) => {
-    current_users.splice(current_users.indexOf(data.user), 1);
-    io.sockets.emit('signout', data);
-   });
+        pagenumbers.insert(entry, find_and_emit_min);
+      });
    
-   
-});
+      socket.on('pagenumber_reset_button', (data) => {
+       var pagenumbers = db.collection('pagenumbers');   
+       //Delete all stored pagenumbers
+       pagenumbers.remove({}, (err, obj) => {});
+       
+      });
+     
+      //Handle new logins
+      socket.on('login', (data) => {
+       current_users.push(data.user);
+       io.sockets.emit('new_user', data);
+      });
+      socket.on('signout', (data) => {
+       current_users.splice(current_users.indexOf(data.user), 1);
+       io.sockets.emit('signout', data);
+      });
+   });
+}
 //Use io.sockets.emit to emit to all sockets
 
 
