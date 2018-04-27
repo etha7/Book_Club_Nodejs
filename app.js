390e2cd2248aedffc24f7c6fbf8032938daf6318
Ethan DeSon <ethandeson@gmail.com>
2018-04-26 21:25:19 -0700

    fixed bug where pagenumbers weren't properly sorted because the sorting was relative to the incorrect field 'pagenumbers' rather than the correct 'pagenumber'

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //Server Code
 const port = process.env.PORT || 3000;
 const hostname = 'localhost';
-
 const express = require('express');
 var app = express();
-//server.listen(80);
-//const port = 3000;
 var path = require('path');
+const favicon = require('serve-favicon');
+
+app.use(favicon(path.join(__dirname,'static','images','favicon.ico')));
 app.use(express.static(__dirname +'/static'));
 app.set('views', path.join(__dirname, 'views'));
 
@@ -47,7 +47,25 @@ function run_app(){
    //TEMP DATA
    var speaker_str = "Default"; //Stores current speaker
    var current_users = [];
-   
+   var pagenumber_semaphore = { num_procs: 0 };  
+   function acquire_lock() {
+      while(pagenumber_semaphore.num_procs != 0){
+      }
+      pagenumber_semaphore.num_procs -= 1;
+   }
+   function release_lock(){
+      pagenumber_semaphore.num_procs += 1;
+   }
+   //Locking wrapper function
+   function lock_w(f){
+     var wrapped = (data) => {
+       acquire_lock();
+       f(data);
+       release_lock();
+    };
+    return wrapped;
+   }
+
    //Handle server/client events:
    io.on('connection', function(socket) {
    
@@ -100,44 +118,53 @@ function run_app(){
       });
    
       //Initliaze pagenumber
-      var pagenumbers = db.collection('pagenumbers'); 
-      //Find and send min page number
-      pagenumbers.find({}).sort({'pagenumbers': 1}).limit(1).toArray((err, result)=> {
-            if(err) throw err;
-            if (result.length == 0) {
-               data = { pagenumber: 0 }; 
-            }
-            else {
-               data = { pagenumber: result[0]['pagenumber'] };
-            }
-            io.sockets.emit('new_lowest_pagenumber', data);
-      });
+      var init_pagenumber = (data) => {
+         var pagenumbers = db.collection('pagenumbers'); 
+
+         //Find and send min page number
+         pagenumbers.find({}).sort({'pagenumber': 1}).limit(1).toArray((err, result)=> {
+               if(err) throw err;
+               if (result.length == 0) {
+                  data = { pagenumber: 0 }; 
+               }
+               else {
+                  data = { pagenumber: result[0]['pagenumber'] };
+               }
+               io.sockets.emit('new_lowest_pagenumber', data);
+         });
+      };
+      lock_w(init_pagenumber)();
      
       //Handle pagenumber submittions
-      socket.on('pagenumber_button', (data) => {
-        //BROKEN BECAUSE pagenumber_button events are affecting each other
-        var pagenumber = data.pagenumber;
-        var entry = { 
-                     'pagenumber': pagenumber
-                    };
-        var find_and_emit_min = () => { 
-           pagenumbers.find({}).sort({'pagenumbers': 1}).limit(1).toArray((err, result)=> { 
-              var new_min = result[0]['pagenumber']; 
-              if(pagenumber <= new_min){
-                 io.sockets.emit('new_lowest_pagenumber', data);
-              }
-           });
-        }
-        pagenumbers.insert(entry, find_and_emit_min);
-      });
+      socket.on('pagenumber_button', lock_w((data) => {
+           //BROKEN BECAUSE pagenumber_button events are affecting each other
+           console.log('pagenumber:');
+           console.log(data.pagenumber);
+           var pagenumbers = db.collection('pagenumbers');
+           var pagenumber = data.pagenumber;
+           var entry = { 
+                        'pagenumber': pagenumber
+                       };
+           var find_and_emit_min = () => { 
+              pagenumbers.find({}).sort({'pagenumber': 1}).limit(1).toArray((err, result)=> { 
+                 var new_min = result[0]['pagenumber']; 
+                 if(pagenumber <= new_min){
+                    io.sockets.emit('new_lowest_pagenumber', data);
+                 }
+                 console.log('finished '+data.pagenumber);
+              });
+           }
+           pagenumbers.insert(entry, find_and_emit_min);
+         })
+      ); 
    
-      socket.on('pagenumber_reset_button', (data) => {
-       var pagenumbers = db.collection('pagenumbers');   
-       //Delete all stored pagenumbers
-       pagenumbers.remove({}, (err, obj) => {});
-       
-      });
-     
+      socket.on('pagenumber_reset_button', lock_w( (data) => {
+          console.log('reset');
+          var pagenumbers = db.collection('pagenumbers');   
+          //Delete all stored pagenumbers
+          pagenumbers.remove({}, (err, obj) => {console.log('finished reset')});
+         })
+      ); 
       //Handle new logins
       socket.on('login', (data) => {
        current_users.push(data.user);
